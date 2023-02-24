45d714d7ea8ca43982d6a11eae8b16c2e7d94502
aczarkowski <aczarkowski@vassar.edu>
2023-02-23 20:42:34 -0500

    Made constructor for Choice

--- src/src/Choice.java
+++ src/src/Choice.java
@@ -1,4 +1,9 @@
 public class Choice {
     String name;
     boolean correct;
+
+    public Choice(String name, boolean correct){
+        this.name = name;
+        this.correct = correct;
+    }
 }
