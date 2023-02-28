1afb499ddbf0c6f39ddcf5cfed542159f813bff0
Pablo CrisostomoSuarez <pcrisostomosuarez@mail.cs.vassar.edu>
2023-02-28 15:51:58 -0500

    Added new TextUI class and edited Game

--- src/src/Game.java
+++ src/src/Game.java
@@ -1,3 +1,5 @@
+package src.src;
+
 import src.src.Player;
 import src.src.Question;
 import src.src.RandMultiChoice;
@@ -12,63 +14,50 @@ public class Game {
         this.numGames = numGames;
     }
 
-    public static void main(String[] args) {
-        Game g = new Game(1);
-        int gcount = 0;
-        while(gcount < g.numGames){
-            g.playSingleGame();
-            gcount++;
-        }
-        //
-        // Player p1 = new Player();
-        // RandMultiChoice gameshow = new RandMultiChoice();
-        // g.correctRecord = 0;
-        // while (g.numGames != g.correctRecord || g.correct) {
 
-        // }
-        // if (g.correct) {
-        // System.out.println("Congratulations");
-        // } else {
-        // System.out.println("Failure");
-        // }
-    }
-
-    public void checkAnswer(Question q, int index){
+    public boolean checkAnswer(Question q, int index){
         Choice c = q.choices.get(index);
-        if (c.correct){
-            this.correctRecord++;
-            System.out.println("That's Right!!!");
-        }
-        else{
-            this.correct = false;
-            Choice realAns = null;
-            char rindx = 65;
-            for (int i = 0; i < q.choices.size(); i++){
-                Choice search = q.choices.get(i);
-                if (search.correct){
-                    realAns = search;
-                    rindx += i;
-                    break;
-                }
-            }
-            System.out.println("Sorry wrong answer. The correct response was " + rindx + ". " + realAns.toString());
-        }
+        return c.correct;
+//        if (c.correct){
+//            this.correctRecord++;
+//            System.out.println("That's Right!!!");
+//        }
+//        else{
+//            this.correct = false;
+//            Choice realAns = null;
+//            char rindx = 65;
+//            for (int i = 0; i < q.choices.size(); i++){
+//                Choice search = q.choices.get(i);
+//                if (search.correct){
+//                    realAns = search;
+//                    rindx += i;
+//                    break;
+//                }
+//            }
+//            System.out.println("Sorry wrong answer. The correct response was " + rindx + ") " + realAns.toString());
+//        }
     }
 
-    public void playSingleGame(){
+    public boolean playSingleGame() {
         Player p1 = new Player();
         //QuestionPool qp = new QuestionPool();
         QuestionPool qp = new QuestionPool();
-        while((this.correct) && (this.correctRecord < 5)){
+        while ((this.correct) && (this.correctRecord < 5)) {
             Question qt = qp.getQuestion();
-            System.out.println(qt.toString());
+            System.out.println(qt);
             int ans = p1.getAnswer();
             this.checkAnswer(qt, ans);
         }
-        if (!this.correct){
-            System.out.print("GAME OVER" + "\nYou got " + correctRecord+ " question(s) right! Better luck next time!");
-        }
-        else{
-            System.out.print("Congratulations!! You successfully answered all " + correctRecord + " questions! You're a trivia master!");
-        }
+        return this.correct;
+        /*if (!this.correct) {
+            S
+        } else {
+
+        }*/
     }
+
+    public void resetGame(){
+        correct = true;
+        correctRecord = 0;
+    }
+}
