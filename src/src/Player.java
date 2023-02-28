876cc03415857b5f1587beeb515c33ba4c91185d
aczarkowski <aczarkowski@vassar.edu>
2023-02-28 16:23:07 -0500

    removed unecessary method from Player

--- src/src/Player.java
+++ src/src/Player.java
@@ -14,23 +14,5 @@ public class Player {
         this.name = "Player 1";
     }
 
-    public int getAnswer() {
-        Scanner scan = new Scanner(System.in);
-        while (true) {
-            String ans = scan.nextLine();
-            ans = ans.toUpperCase();
-            switch (ans) {
-                case "A":
-                    return 0;
-                case "B":
-                    return 1;
-                case "C":
-                    return 2;
-                case "D":
-                    return 3;
-                default:
-                    System.out.println("Invalid Answer, Please try again.");
-            }
-        }
-    }
+
 }
