6cc84577c41cedcc581e89a7e062826b98709063
aczarkowski <aczarkowski@vassar.edu>
2023-02-28 16:17:42 -0500

    Made edits to TextUTI

--- src/src/Game.java
+++ src/src/Game.java
@@ -14,50 +14,66 @@ public class Game {
         this.numGames = numGames;
     }
 
+    // public static void main(String[] args) {
+    // Game g = new Game(1);
+    // int gcount = 0;
+    // while(gcount < g.numGames){
+    // g.playSingleGame();
+    // gcount++;
+    // }
+    // //
+    // // Player p1 = new Player();
+    // // RandMultiChoice gameshow = new RandMultiChoice();
+    // // g.correctRecord = 0;
+    // // while (g.numGames != g.correctRecord || g.correct) {
 
-    public boolean checkAnswer(Question q, int index){
+    // // }
+    // // if (g.correct) {
+    // // System.out.println("Congratulations");
+    // // } else {
+    // // System.out.println("Failure");
+    // // }
+    // }
+
+    public String checkAnswer(Question q, int index){
         Choice c = q.choices.get(index);
-        return c.correct;
-//        if (c.correct){
-//            this.correctRecord++;
-//            System.out.println("That's Right!!!");
-//        }
-//        else{
-//            this.correct = false;
-//            Choice realAns = null;
-//            char rindx = 65;
-//            for (int i = 0; i < q.choices.size(); i++){
-//                Choice search = q.choices.get(i);
-//                if (search.correct){
-//                    realAns = search;
-//                    rindx += i;
-//                    break;
-//                }
-//            }
-//            System.out.println("Sorry wrong answer. The correct response was " + rindx + ") " + realAns.toString());
-//        }
-    }
-
-    public boolean playSingleGame() {
-        Player p1 = new Player();
-        //QuestionPool qp = new QuestionPool();
-        QuestionPool qp = new QuestionPool();
-        while ((this.correct) && (this.correctRecord < 5)) {
-            Question qt = qp.getQuestion();
-            System.out.println(qt);
-            int ans = p1.getAnswer();
-            this.checkAnswer(qt, ans);
+        String s = "";
+        if (c.correct){
+            this.correctRecord++;
+            s = "That's Right!!!";
         }
-        return this.correct;
-        /*if (!this.correct) {
-            S
-        } else {
-
-        }*/
+        else{
+            this.correct = false;
+            Choice realAns = null;
+            char rindx = 65;
+            for (int i = 0; i < q.choices.size(); i++){
+                Choice search = q.choices.get(i);
+                if (search.correct){
+                    realAns = search;
+                    rindx += i;
+                    break;
+                }
+            }
+            s = "Sorry wrong answer. The correct response was " + rindx + ". " + realAns.toString();
+        }
+        return s;
     }
 
-    public void resetGame(){
-        correct = true;
-        correctRecord = 0;
-    }
+    // public void playSingleGame(){
+    // Player p1 = new Player();
+    // //QuestionPool qp = new QuestionPool();
+    // QuestionPool qp = new QuestionPool();
+    // while((this.correct) && (this.correctRecord < 5)){
+    // Question qt = qp.getQuestion();
+    // System.out.println(qt.toString());
+    // int ans = p1.getAnswer();
+    // //this.checkAnswer(qt, ans);
+    // }
+    // if (!this.correct){
+    // System.out.print("GAME OVER" + "\nYou got " + correctRecord+ " question(s) right! Better luck next time!");
+    // }
+    // else{
+    // System.out.print("Congratulations!! You succesfully answered all " + correctRecord + " questions! You're a trivia master!");
+    // }
+    // }
 }
--- src/src/TextUI.java
+++ src/src/TextUI.java
@@ -3,42 +3,52 @@ package src.src;
 import java.util.Scanner;
 
 public class TextUI {
-    public static void main(String args[]){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to Pablo and Alex's CMPU 203 Trivia Game!\n Try answering 5 questions correctly in a row");
+    public static void main(String[] args) {
+        TextUI ui = new TextUI();
         Game g = new Game(1);
         int gcount = 0;
         while(gcount < g.numGames){
-            boolean hasPlayerWon = g.playSingleGame();
-            boolean isCorrectAnswer = g.checkAnswer();
-            if (isCorrectAnswer){
-                g.correctRecord++;
-                System.out.println("That's Right!!!");
-            }
-            else{
-                g.correct = false;
-                Choice realAns = null;
-                char rindx = 65;
-                for (int i = 0; i < q.choices.size(); i++){
-                    Choice search = q.choices.get(i);
-                    if (search.correct){
-                        realAns = search;
-                        rindx += i;
-                        break;
-                    }
-                }
-                System.out.println("Sorry wrong answer. The correct response was " + rindx + ") " + realAns.toString());
-            }
-
-            if(hasPlayerWon){
-                System.out.println("Congratulations!! You successfully answered all " + g.correctRecord + " questions! You're a trivia master!");
-            }else{
-                System.out.println("GAME OVER" + "\nYou got " + g.correctRecord + " question(s) right! Better luck next time!");
-            }
-            g.resetGame();
+            ui.playSingleGame(g);
             gcount++;
         }
+    }
 
+    public void playSingleGame(Game g){
 
+        Player p1 = new Player();
+        //QuestionPool qp = new QuestionPool();
+        QuestionPool qp = new QuestionPool();
+        while((g.correct) && (g.correctRecord < 5)){
+            Question qt = qp.getQuestion();
+            System.out.println(qt.toString());
+            int ans = p1.getAnswer();
+            System.out.println(g.checkAnswer(qt, ans));
+        }
+        if (!g.correct){
+            System.out.print("GAME OVER" + "\nYou got " + g.correctRecord+ " question(s) right! Better luck next time!");
+        }
+        else{
+            System.out.print("Congratulations!! You succesfully answered all " + g.correctRecord + " questions! You're a trivia master!");
+        }
+    }
+
+    public int getAnswer() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            String ans = scan.nextLine();
+            ans = ans.toUpperCase();
+            switch (ans) {
+                case "A":
+                    return 0;
+                case "B":
+                    return 1;
+                case "C":
+                    return 2;
+                case "D":
+                    return 3;
+                default:
+                    System.out.println("Invalid Answer, Please try again.");
+            }
+        }
     }
 }
