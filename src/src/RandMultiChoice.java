3e451f5533f510f900fc004f40db83e773f4d5de
Pablo CrisostomoSuarez <pcrisostomosuarez@turing.cs.vassar.edu>
2023-02-28 00:44:08 -0500

    Updated RandMultiChoice

--- src/src/RandMultiChoice.java
+++ src/src/RandMultiChoice.java
@@ -1,8 +1,6 @@
 package src.src;
 public class RandMultiChoice implements IGameShow {
     public Question pullQuestion(QuestionPool qp){
-        Question q = qp.getQuestion();
-        qp.removeQuestion(q);
-        return q;
+        return qp.getQuestion();
     }
 }
