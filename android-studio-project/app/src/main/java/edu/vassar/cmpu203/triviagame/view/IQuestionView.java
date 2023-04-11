fc0cb25d302a0f13bd5e2ee8c75769803ae101a6
pcrisostomosuarez <pcrisostomosuarez@vassar.edu>
2023-04-11 15:48:11 -0400

    Added IQuestionView.java

---
+++ android-studio-project/app/src/main/java/edu/vassar/cmpu203/triviagame/view/IQuestionView.java
@@ -0,0 +1,15 @@
+package edu.vassar.cmpu203.triviagame.view;
+
+import androidx.annotation.NonNull;
+
+public interface IQuestionView {
+
+    interface Listener {
+
+
+        void onAnswerChoice();
+
+        void onSubmit();
+
+    }
+}
