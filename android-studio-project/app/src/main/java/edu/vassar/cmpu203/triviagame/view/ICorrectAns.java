b7f88ac0d4715bb06eb694897ef3671b6d66475e
aczarkowski <aczarkowski@vassar.edu>
2023-04-11 15:50:05 -0400

    Made two more interfaces for app

---
+++ android-studio-project/app/src/main/java/edu/vassar/cmpu203/triviagame/view/ICorrectAns.java
@@ -0,0 +1,14 @@
+package edu.vassar.cmpu203.triviagame.view;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+public interface ICorrectAns {
+    interface Listener {
+
+        void onNext(); // called when user wants to go to next question
+
+
+    }
+}
---
+++ android-studio-project/app/src/main/java/edu/vassar/cmpu203/triviagame/view/IGameMode.java
@@ -0,0 +1,16 @@
+package edu.vassar.cmpu203.triviagame.view;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+public interface IGameMode {
+
+    interface Listener {
+
+        void onGoBack(); // called when user wants to go back to Menu screen
+
+
+    }
+
+}
