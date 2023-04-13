69596040602d28b65b40aeb43858bd5153a8bd64
pcrisostomosuarez <pcrisostomosuarez@vassar.edu>
2023-04-13 16:19:53 -0400

    Change QuestionFragment

--- android-studio-project/app/src/main/java/edu/vassar/cmpu203/triviagame/view/QuestionFragment.java
+++ android-studio-project/app/src/main/java/edu/vassar/cmpu203/triviagame/view/QuestionFragment.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import edu.vassar.cmpu203.triviagame.R;
 import edu.vassar.cmpu203.triviagame.databinding.FragmentQuestionBinding;
@@ -16,6 +18,9 @@ import edu.vassar.cmpu203.triviagame.databinding.FragmentQuestionBinding;
 
 public class QuestionFragment extends Fragment implements IQuestionView{
     private FragmentQuestionBinding binding;
+
+
+
     private Listener listener;
 
     public QuestionFragment(){
@@ -24,12 +29,24 @@ public class QuestionFragment extends Fragment implements IQuestionView{
 
     public QuestionFragment(Listener listener){
         this.listener = listener;
+        //questionPrompt = (TextView) getView().findViewById(R.id.questiontext);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         this.binding = FragmentQuestionBinding.inflate(inflater);
+        //questionPrompt
+        TextView questionPrompt = this.binding.getRoot().findViewById(R.id.questiontext);
+        questionPrompt.setText("This has changed");
+        RadioButton choice1 = this.binding.getRoot().findViewById(R.id.radioButton);
+        choice1.setText("choice1");
+        RadioButton choice2 = this.binding.getRoot().findViewById(R.id.radioButton2);
+        choice2.setText("choice2");
+        RadioButton choice3 = this.binding.getRoot().findViewById(R.id.radioButton3);
+        choice3.setText("choice3");
+        RadioButton choice4 = this.binding.getRoot().findViewById(R.id.radioButton4);
+        choice4.setText("choice4");
         return this.binding.getRoot();
     }
 
