37b6764434b87055e46a6cabfd6f7b2867485510
pcrisostomosuarez <pcrisostomosuarez@vassar.edu>
2023-05-02 16:00:34 -0400

    Updated Player class to keep track of category wins;

--- android-studio-project/app/src/main/java/edu/vassar/cmpu203/triviagame/model/Player.java
+++ android-studio-project/app/src/main/java/edu/vassar/cmpu203/triviagame/model/Player.java
@@ -1,10 +1,14 @@
 package edu.vassar.cmpu203.triviagame.model;
 
+import java.util.HashMap;
+
 public class Player {
     String name;
     int wins = 0;
     public int answerStreak = 0;
     public int questionNumber = 1;
+
+    public HashMap<String, Integer> categoryScores = new HashMap<>();
 
     public Player(String name) {
         this.name = name;
@@ -28,6 +32,11 @@ public class Player {
     public void resetStreak(){
         answerStreak = 0;
         questionNumber = 1;
+    }
+
+    public void addCategoryWin(String category){
+        Integer score = categoryScores.get(category);
+        categoryScores.put(category,score + 1);
     }
 
 }
